3922881744b36c794a4ea935adb407fc305b0cbc
Simon <122997456+moodleismylms@users.noreply.github.com>
2023-06-01 16:31:56 +0200

    cerate basic files

---
+++ backup/moodle2/restore_upc_stepslib.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * All the steps to restore mod_upc are defined here.
+ *
+ * @package     mod_upc
+ * @category    backup
+ * @copyright   2023 Simon Schaudt  and others
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// More information about the backup process: {@link https://docs.moodle.org/dev/Backup_API}.
+// More information about the restore process: {@link https://docs.moodle.org/dev/Restore_API}.
+
+/**
+ * Defines the structure step to restore one mod_upc activity.
+ */
+class restore_upc_activity_structure_step extends restore_activity_structure_step {
+
+    /**
+     * Defines the structure to be restored.
+     *
+     * @return restore_path_element[].
+     */
+    protected function define_structure() {
+        $paths = array();
+        $userinfo = $this->get_setting_value('userinfo');
+
+        return $this->prepare_activity_structure($paths);
+    }
+
+    /**
+     * Defines post-execution actions.
+     */
+    protected function after_execute() {
+        return;
+    }
+}
