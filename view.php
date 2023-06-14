243a024a7a55fadb9e71b2d070ca13d7049706cc
simon2023 <simon.schaudt@ph-weingarten.de>
2023-06-14 09:57:01 +0200

    render templates from view.php

--- view.php
+++ view.php
@@ -61,10 +61,15 @@ $PAGE->set_context($modulecontext);
 $templatesettingscard = (object)[
     'url' => '../../25_Steal_Falls.jpg',
     'text' => 'testtext',
+    'name' => 'testname',
+    'edit' => ,
+    'delete' => ,
+    'export' => ,
 ];
 
 echo $OUTPUT->header();
 
 echo $OUTPUT->render_from_template('mod_upc/card', $templatesettingscard);
+echo $OUTPUT->render_from_template('mod_upc/new_card' $templatesettingscard);
 
 echo $OUTPUT->footer();
