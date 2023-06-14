97dc0d2e72b15ea4a8e895c2d86ae5b13bd5cccc
Marius Rosenbaum <xianahru@gmx.net>
2023-06-14 12:12:16 +0200

    added check if a card is your own

--- view.php
+++ view.php
@@ -26,7 +26,7 @@ require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
 require_once(__DIR__.'/locallib.php');
 
-global $USER;
+global $USER, $DB;
 
 // Course module id.
 $id = optional_param('id', 0, PARAM_INT);
@@ -69,10 +69,7 @@ $newcarddata = array(
 
 echo $OUTPUT->render_from_template('mod_upc/new_card', $newcarddata);
 
-global $DB;
-
 $context = context_module::instance($cm->id);
-
 
 $records = $DB->get_records('userdata', array('activityid' => $context->instanceid));
 foreach ($records as $record) {
@@ -81,6 +78,7 @@ foreach ($records as $record) {
         'url' => get_image_link($context->id, $record->userid),
         'text' => $record->textfield,
         'name' => $user->firstname . ' ' . $user->lastname,
+        'its_my_card' => $user->id === $USER->id
     ];
     echo $OUTPUT->render_from_template('mod_upc/card', $templatesettingscard);
 }
